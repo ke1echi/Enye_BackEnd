8eafd57c7db30a91fbb4609c74b1ff82146bb8d6
ke1echi <mrkelechichinaka@gmail.com>
2021-01-20 12:49:29 +0100

    Deploying code to production

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send(`Server up and running on port ${PORT}!`);
+});
+
 // Use route
 const controllerRoute = require('./src/router/index');
 app.use(controllerRoute);
